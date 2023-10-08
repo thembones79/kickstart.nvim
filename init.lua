3a97c77873ceed7cc799c90b7ce23a5ed1c99c42
Michał Szumnarski <michal.szumnarski@gmail.com>
2023-10-08 17:18:26 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -151,11 +151,10 @@ require('lazy').setup({
   },
 
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
 
@@ -166,7 +165,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'gruvbox',
         component_separators = '|',
         section_separators = '',
       },
