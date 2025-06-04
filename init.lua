edb83a76e11ac9ddeb7412a3de731a988f60551b
thembones79 <michal.szumnarski@gmail.com>
2025-06-04 21:15:58 +0200

    refactor

--- init.lua
+++ init.lua
@@ -872,7 +872,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, ts = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -888,7 +888,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
