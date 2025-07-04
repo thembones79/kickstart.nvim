97d1c55cc2e9b166dd602f9b71d3046bd53abd47
Michał Szumnarski <michal.szumnarski@gmail.com>
2025-07-04 15:20:35 +0200

    Update init.lua. - windows compatibility

--- init.lua
+++ init.lua
@@ -122,7 +122,7 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.opt.undodir = '~/.vim/undodir'
 vim.opt.undofile = true
 
 -- vim.opt.hlsearch = false
